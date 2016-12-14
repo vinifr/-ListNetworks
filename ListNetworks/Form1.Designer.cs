818fbaa02f19804a85138a1896eb121b5f89cc4a
Vinicius Maciel <viniciusfre@gmail.com>
2016-12-13 22:34:13 -0300

    First commit

---
+++ ListNetworks/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace ListNetworks
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstBoxPrinters = new System.Windows.Forms.ListBox();
+            this.btSearchNetPrinter = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstBoxPrinters
+            // 
+            this.lstBoxPrinters.FormattingEnabled = true;
+            this.lstBoxPrinters.Location = new System.Drawing.Point(23, 57);
+            this.lstBoxPrinters.Name = "lstBoxPrinters";
+            this.lstBoxPrinters.Size = new System.Drawing.Size(637, 212);
+            this.lstBoxPrinters.TabIndex = 0;
+            // 
+            // btSearchNetPrinter
+            // 
+            this.btSearchNetPrinter.Location = new System.Drawing.Point(197, 289);
+            this.btSearchNetPrinter.Name = "btSearchNetPrinter";
+            this.btSearchNetPrinter.Size = new System.Drawing.Size(101, 36);
+            this.btSearchNetPrinter.TabIndex = 1;
+            this.btSearchNetPrinter.Text = "Search";
+            this.btSearchNetPrinter.UseVisualStyleBackColor = true;
+            this.btSearchNetPrinter.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(376, 289);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 36);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(681, 384);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btSearchNetPrinter);
+            this.Controls.Add(this.lstBoxPrinters);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstBoxPrinters;
+        private System.Windows.Forms.Button btSearchNetPrinter;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ ListNetworks/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Management;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Net.NetworkInformation;
+using System.Diagnostics;
+using SnmpSharpNet;
+
+namespace ListNetworks
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "Search Net Printers";
+        }
+
+        private string GetLocalIPv4(NetworkInterfaceType _type)
+        {
+            string output = null;
+            foreach (NetworkInterface item in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if (item.NetworkInterfaceType == _type && item.OperationalStatus == OperationalStatus.Up)
+                {
+                    foreach (UnicastIPAddressInformation ip in item.GetIPProperties().UnicastAddresses)
+                    {
+                        if (ip.Address.AddressFamily == AddressFamily.InterNetwork)
+                        {
+                            output = ip.Address.ToString();
+                        }
+                    }
+                }
+            }
+            return output;
+        }
+
+        private bool getSnmp(string host, string OID)
+        {
+            bool result = false;
+            /* Get an SNMP Object
+            */
+            SimpleSnmp snmpVerb = new SimpleSnmp(host, 161, "public", 20, 0);
+            if (!snmpVerb.Valid)
+            {
+                //MessageBox.Show("Seems that IP or comunauty is not cool");
+                return result;
+            }
+
+            Oid varbind = new Oid(OID);
+
+            Dictionary<Oid, AsnType> snmpDataS = snmpVerb.Get(SnmpVersion.Ver1, new string[] { varbind.ToString() });
+            if (snmpDataS != null)
+            {
+                string temp = snmpDataS[varbind].ToString();
+                // Get MANUFACTURER
+                int startIndex = temp.IndexOf("MFG:");
+                int endIndex = temp.IndexOf(";", startIndex);
+                string mfg = temp.Substring(startIndex + 4, endIndex - (startIndex + 4));
+                // Get MODEL
+                startIndex = temp.IndexOf("MDL:");
+                endIndex = temp.IndexOf(";", startIndex);
+                string printerMDL = temp.Substring(startIndex + 4, endIndex - (startIndex + 4));
+
+
+                this.Invoke((MethodInvoker)(() => lstBoxPrinters.Items.Add(
+                    string.Format("{0}:{1} ({2})", host, printerMDL, mfg))));
+                result = true;
+            }
+            else
+                Console.WriteLine("Not Response from " + host);
+
+            return result;
+        }       
+
+        [DllImport("iphlpapi.dll", ExactSpelling = true)]
+        public static extern int SendARP(int DestIP, int SrcIP, byte[] pMacAddr, ref uint PhyAddrLen);
+
+        void SendArpRequest(IPAddress dst)
+        {
+            byte[] macAddr = new byte[6];
+            uint macAddrLen = (uint)macAddr.Length;
+            int uintAddress = BitConverter.ToInt32(dst.GetAddressBytes(), 0);
+
+            if (SendARP(uintAddress, 0, macAddr, ref macAddrLen) == 0)
+            {
+                //Console.WriteLine("{0} responded to ping", dst.ToString());
+                /* PRINTER-PORT-MONITOR-MIB - 1.3.6.1.4.1.2699
+                 * ppmPrinterIEEE1284DeviceId: 1.3.6.1.4.1.2699.1.2.1.2.1.1.3
+                 *
+                 * ppmPrinterName: 1.3.6.1.4.1.2699.1.2.1.2.1.1.2
+                 */
+                getSnmp(dst.ToString(), "1.3.6.1.4.1.2699.1.2.1.2.1.1.3.1");
+            }
+        }
+
+        private void ScanPrinters()
+        {
+           string prefix = null;
+
+            string temp = GetLocalIPv4(NetworkInterfaceType.Ethernet);
+            if (temp != null)
+                prefix = temp.Substring(0, 3);
+            if (temp == null || (prefix == "169"))
+                temp = GetLocalIPv4(NetworkInterfaceType.Wireless80211);
+            string ipBase = temp;
+            try
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    ipBase = temp.Remove(ipBase.Length - 1);
+                    if (ipBase.EndsWith("."))
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Errormessage = " + ex.Message);
+            }
+
+            //Generating IP Range
+            List<IPAddress> ipAddressList = new List<IPAddress>();
+            for (int i = 1; i < 254; i++)
+            {
+                //Obviously you'll want to safely parse user input to catch exceptions.
+                ipAddressList.Add(IPAddress.Parse(ipBase + i));
+            }
+
+            foreach (IPAddress ip in ipAddressList)
+            {
+                Thread thread = new Thread(() => SendArpRequest(ip));
+                thread.Start();
+            }
+            MessageBox.Show("Search End: " + ipBase);
+            btSearchNetPrinter.BeginInvoke((Action)delegate()
+            {
+                btSearchNetPrinter.Enabled = true;
+            });
+        }
+        
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            lstBoxPrinters.Items.Clear();
+            Task task = Task.Run(() => ScanPrinters());
+            await task;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            lstBoxPrinters.Items.Clear();
+        }
+    }
+}
